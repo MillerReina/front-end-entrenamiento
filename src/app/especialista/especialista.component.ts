646c4744ee89b233450f04bb071c0b9bbec53810
Miller Reina <miller.uchamocha@bam.com.gt>
2022-06-25 00:59:32 -0500

    changes

--- src/app/especialista/especialista.component.ts
+++ src/app/especialista/especialista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-especialista',
@@ -7,14 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EspecialistaComponent implements OnInit {
   public isCreating!: boolean;
+  public especialistaForm!: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
     this.isCreating = false;
+    this.especialistaForm = this.fb.group({
+      fechaDeNacimiento: ['', [Validators.required]],
+      nombre: ['', [Validators.required]],
+      registro: [''],
+      tarjetaProfesional: ['', [Validators.required]],
+    });
+  }
+  get fechaDeNacimientoIsInvalid(): boolean {
+    return (this.especialistaForm.get('fechaDeNacimiento')?.invalid &&
+      this.especialistaForm.get('fechaDeNacimiento')?.touched) as boolean;
+  }
+
+  get nombreIsInvalid(): boolean {
+    return (this.especialistaForm.get('nombre')?.invalid &&
+      this.especialistaForm.get('nombre')?.touched) as boolean;
+  }
+
+  get tarjetaIsInvalid(): boolean {
+    return (this.especialistaForm.get('tarjetaProfesional')?.invalid &&
+      this.especialistaForm.get('tarjetaProfesional')?.touched) as boolean;
   }
 
   ngOnInit(): void {}
 
   crearEspecialista() {
     this.isCreating = true;
+  }
+
+  crear() {
+    if (this.especialistaForm.invalid) {
+      this.especialistaForm.markAllAsTouched();
+    } else {
+      console.log('CREANDO');
+    }
+  }
+
+  cancelProcess() {
+    this.isCreating = false;
+    this.especialistaForm.reset();
   }
 }
